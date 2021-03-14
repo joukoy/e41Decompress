e647ca67f0df58d2c772934d7af60c63938cac12
Jouko Y <joukoy@gmail.com>
2021-03-14 15:40:42 +0200

    0.1.1

---
+++ e41Decompress/Helpers.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace e41Decompress
+{
+    public static class Helpers
+    {
+        public static string SelectFile(string Title = "Select file", string Filter = "BIN files (*.bin)|*.bin|All files (*.*)|*.*")
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = Title;
+            fdlg.Filter = Filter;
+            fdlg.FilterIndex = 1;
+            fdlg.RestoreDirectory = true;
+
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                return fdlg.FileName;
+            }
+            return "";
+
+        }
+        public static string SelectSaveFile(string Filter = "BIN files (*.bin)|*.bin|All files (*.*)|*.*", string defaultFileName = "")
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = Filter;
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.Title = "Save to file";
+            saveFileDialog.FileName = defaultFileName;
+
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                return saveFileDialog.FileName;
+            }
+            else
+                return "";
+
+        }
+
+        public static string SelectFolder(string Title, string defaultFolder = "")
+        {
+            string folderPath = "";
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            // Set validate names and check file exists to false otherwise windows will
+            // not let you select "Folder Selection."
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            folderBrowser.InitialDirectory = defaultFolder;
+            // Always default to Folder Selection.
+            folderBrowser.Title = Title;
+            folderBrowser.FileName = "Folder Selection";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = Path.GetDirectoryName(folderBrowser.FileName);
+            }
+            return folderPath;
+        }
+
+        public static byte[] ReadBin(string FileName, uint FileOffset, uint Length)
+        {
+
+            byte[] buf = new byte[Length];
+
+            long offset = 0;
+            long remaining = Length;
+
+            using (BinaryReader freader = new BinaryReader(File.Open(FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)))
+            {
+                freader.BaseStream.Seek(FileOffset, 0);
+                while (remaining > 0)
+                {
+                    int read = freader.Read(buf, (int)offset, (int)remaining);
+                    if (read <= 0)
+                        throw new EndOfStreamException
+                            (String.Format("End of stream reached with {0} bytes left to read", remaining));
+                    remaining -= read;
+                    offset += read;
+                }
+                freader.Close();
+            }
+            return buf;
+        }
+        public static void WriteBinToFile(string FileName, byte[] Buf)
+        {
+
+            using (FileStream stream = new FileStream(FileName, FileMode.Create))
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    writer.Write(Buf);
+                    writer.Close();
+                }
+            }
+        }
+        public static uint BEToUint32(byte[] buf, uint offset)
+        {
+            //Shift first byte 24 bits left, second 16bits left...
+            return (uint)((buf[offset] << 24) | (buf[offset + 1] << 16) | (buf[offset + 2] << 8) | buf[offset + 3]);
+        }
+
+        public static UInt16 BEToUint16(byte[] buf, uint offset)
+        {
+            return (UInt16)((buf[offset] << 8) | buf[offset + 1]);
+        }
+
+    }
+}
---
+++ e41Decompress/frmFileSelection.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace e41Decompress
+{
+    public partial class frmFileSelection : Form
+    {
+        public frmFileSelection()
+        {
+            InitializeComponent();
+            lvwColumnSorter = new ListViewColumnSorter();
+            this.listFiles.ListViewItemSorter = lvwColumnSorter;
+
+        }
+
+        public string filter = ".bin";
+        private ListViewColumnSorter lvwColumnSorter;
+
+        public void LoadFiles(string Folder)
+        {
+            listFiles.Enabled = true;
+            listFiles.Items.Clear();
+            listFiles.View = View.Details;
+            listFiles.Columns.Add("File");
+            listFiles.Columns.Add("Folder");
+            listFiles.Columns.Add("Size");
+            listFiles.Columns[0].Width = 300;
+            listFiles.Columns[1].Width = 300;
+            listFiles.Columns[2].Width = 200;
+
+            listFiles.CheckBoxes = true;
+            if (Folder == "")
+                Folder = Application.StartupPath;
+            DirectoryInfo d = new DirectoryInfo(Folder);
+            FileInfo[] Files;
+            if (chkSubfolders.Checked)
+                Files = d.GetFiles("*.*", SearchOption.AllDirectories);
+            else
+                Files = d.GetFiles("*.*");
+
+            string[] filters;
+            filters = new string[1];
+            filters[0] = filter;
+            foreach (FileInfo file in Files)
+            {
+                for (int f = 0; f < filters.Length; f++)
+                {
+                    if (file.Extension.ToLower() == filters[f].ToLower())
+                    {
+                        var item = new ListViewItem(file.Name);
+                        item.Tag = file.FullName;
+                        item.SubItems.Add(file.DirectoryName);
+                        string filesize = file.Length.ToString();
+                        //if (file.Length > 1023)
+                          //  filesize = ((int)(file.Length / 1024)).ToString() + " k";
+                        item.SubItems.Add(filesize);
+                        listFiles.Items.Add(item);
+                        break;
+                    }
+                }
+            }
+            txtFolder.Text = Folder;
+        }
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            string Folder = Helpers.SelectFolder("Select folder", txtFolder.Text);
+            if (Folder.Length > 0)
+                LoadFiles(Folder);
+        }
+
+
+        private void txtFolder_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                LoadFiles(txtFolder.Text);
+
+        }
+
+
+        private void chkSubfolders_CheckedChanged(object sender, EventArgs e)
+        {
+            LoadFiles(txtFolder.Text);
+        }
+
+        private void chkIncludeCustomFileTypes_CheckedChanged(object sender, EventArgs e)
+        {
+            LoadFiles(txtFolder.Text);
+        }
+
+        private void frmFileSelection_Load(object sender, EventArgs e)
+        {
+            listFiles.ColumnClick += ListFiles_ColumnClick;
+        }
+
+        private void ListFiles_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            // Determine if clicked column is already the column that is being sorted.
+            if (e.Column == lvwColumnSorter.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                if (lvwColumnSorter.Order == SortOrder.Ascending)
+                {
+                    lvwColumnSorter.Order = SortOrder.Descending;                    
+                }
+                else
+                {
+                    lvwColumnSorter.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                lvwColumnSorter.SortColumn = e.Column;
+                lvwColumnSorter.Order = SortOrder.Ascending;
+            }
+
+            // Perform the sort with these new sort options.
+            this.listFiles.Sort();
+        }
+
+        private void frmFileSelection_FormClosing(object sender, EventArgs e)
+        {
+        }
+
+        private void chkSelectAll_CheckedChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listFiles.Items.Count; i++)
+            {
+                if (chkSelectAll.Checked)
+                    listFiles.Items[i].Checked = true;
+                else
+                    listFiles.Items[i].Checked = false;
+            }
+
+        }
+    }
+}
